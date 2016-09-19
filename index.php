5839aaa1d63c634d916576ecff5891d5f8214c04
Дмитрий Яковлев <dokiz_228@mail.ru>
2016-09-19 17:44:40 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,24 @@
 <meta charset="Utf-8">
 <?php
-$namesList[0]="Апельсин";
-$namesList[1]="Банан";
-$namesList[2]="Груша";
-$namesList[3]="Помидор";
-$namesList[4]="Огурец";
-echo $names[0]; 
+$names[0]="Апельсин";
+$names[1]="Банан";
+$names[2]="Груша";
+$names[3]="Помидор";
+$names[4]="Огурец";
 echo "<br>";
-echo $names[3];
-echo "<br>";
-echo $names[2];  
-echo "<br>";
-echo $names[1];
-echo "<br>";
-echo $names[4];  
+echo "<pre>";
+print_r($names);
+echo "</pre>";
+
+
+//
+///
+//
+$names[0]="Апельсин";
+$names[1]="Банан";
+$names[2]="Груша";
+list($names[0], $names[1], $names[2]) = $names;
+print_r($names)
+
+
 ?>
